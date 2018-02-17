84b43b462d910f83f14faec94ab7981880864ec2
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2018-02-17 23:09:53 +0100

    Added example

---
+++ Xamarin.ExifLocation/GPS.cs
@@ -0,0 +1,183 @@
+﻿using Java.Lang;
+
+namespace Xamarin.ExifLocation
+{
+    public class GPS
+    {
+        private static StringBuilder sb = new StringBuilder(20);
+        /**
+        * returns ref for latitude which is S or N.
+        *
+        * @param latitude
+        * @return S or N
+*/
+        public static string LatitudeRef(double latitude)
+        {
+            return latitude < 0.0d ? "S" : "N";
+        }
+        /**
+        * returns ref for latitude which is S or N.
+        *
+        * @param latitude
+        * @return S or N
+*/
+        public static string LongitudeRef(double longitude)
+        {
+            return longitude < 0.0d ? "W" : "E";
+        }
+        /**
+        * convert latitude into DMS (degree minute second) format. For instance<br/>
+        * -79.948862 becomes<br/>
+        * 79/1,56/1,55903/1000<br/>
+        * It works for latitude and longitude<br/>
+        *
+        * @param latitude could be longitude.
+        * @return
+*/
+        public static string Convert(double value)
+        {
+            value = Math.Abs(value);
+            int degree = (int)value;
+            value *= 60;
+            value -= degree * 60.0d;
+            int minute = (int)value;
+            value *= 60;
+            value -= minute * 60.0d;
+            int second = (int)(value * 1000.0d);
+            sb.SetLength(0);
+            sb.Append(degree);
+            sb.Append("/1,");
+            sb.Append(minute);
+            sb.Append("/1,");
+            sb.Append(second);
+            sb.Append("/1000,");
+            return sb.ToString();
+        }
+    }
+
+
+    //public class MediaHelper
+    //{
+    //    public static string GetPath(Context context, Uri uri)
+    //    {
+    //        bool isKitKat = Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat;
+
+    //        // DocumentProvider
+    //        if (isKitKat && DocumentsContract.IsDocumentUri(context, uri))
+    //        {
+    //            // ExternalStorageProvider
+    //            if (isExternalStorageDocument(uri))
+    //            {
+    //                var docId = DocumentsContract.GetDocumentId(uri);
+    //                var split = docId.Split(':');
+    //                var type = split[0];
+
+    //                if ("primary".Equals(type, StringComparison.InvariantCultureIgnoreCase))
+    //                {
+    //                    return Environment.ExternalStorageDirectory + "/" + split[1];
+    //                }
+    //                // TODO handle non-primary volumes
+    //            }
+    //            // DownloadsProvider
+    //            else if (IsDownloadsDocument(uri))
+    //            {
+    //                var id = DocumentsContract.GetDocumentId(uri);
+    //                var contentUri = ContentUris.WithAppendedId(Uri.Parse("content://downloads/public_downloads"), long.Parse(id));
+    //                return GetDataColumn(context, contentUri, null, null);
+    //            }
+    //            // MediaProvider
+    //            else
+    //            if (IsMediaDocument(uri))
+    //            {
+    //                var docId = DocumentsContract.GetDocumentId(uri);
+    //                var split = docId.Split(':');
+    //                var type= split[0];
+    //                Uri contentUri = null;
+    //                switch (type)
+    //                {
+    //                    case "image":
+    //                    case "video":
+    //                    case "audio":
+    //                        contentUri = MediaStore.Images.Media.ExternalContentUri;
+    //                        break;
+    //                    default:
+    //                        break;
+    //                }
+
+    //                var selection = "_id=?";
+    //                var selectionArgs = new [] { split[1] };
+    //                return GetDataColumn(context, contentUri, selection, selectionArgs);
+    //            }
+    //        }
+    //        // MediaStore (and general)
+    //        else if ("content".Equals(uri.Scheme, StringComparison.InvariantCultureIgnoreCase))
+    //        {
+    //            // Return the remote address
+    //            if (IsGooglePhotosUri(uri))
+    //                return uri.LastPathSegment;
+    //            return GetDataColumn(context, uri, null, null);
+    //        }
+    //        // File
+    //        else if ("file".Equals(uri.Scheme, StringComparison.InvariantCultureIgnoreCase))
+    //        {
+    //            return uri.Path;
+    //        }
+    //        return null;
+    //    }
+
+    //    public static string GetDataColumn(Context context, Uri uri, string selection, string[] selectionArgs)
+    //    {
+    //        ICursor cursor = null;
+    //        var column = MediaStore.Images.Media.InterfaceConsts.Data;
+    //        var projection = new[] { column };
+    //        try
+    //        {
+    //            cursor = context.ContentResolver.Query(uri, projection, selection, selectionArgs, null);
+    //            if (cursor != null && cursor.MoveToFirst())
+    //            {
+    //                var index = cursor.GetColumnIndexOrThrow(column);
+    //                return cursor.GetString(index);
+    //            }
+    //        }
+    //        finally
+    //        {
+    //            if (cursor != null)
+    //                cursor.Close();
+    //        }
+    //        return null;
+    //    }
+
+    //    public static bool isExternalStorageDocument(Uri uri)
+    //    {
+    //        return "com.android.externalstorage.documents".Equals(uri.Authority, StringComparison.InvariantCultureIgnoreCase);
+    //    }
+
+    //    /**
+    //     * @param uri The Uri to check.
+    //     * @return Whether the Uri authority is DownloadsProvider.
+    //     */
+    //    public static bool IsDownloadsDocument(Uri uri)
+    //    {
+    //        return "com.android.providers.downloads.documents".Equals(uri.Authority, StringComparison.InvariantCultureIgnoreCase);
+    //    }
+
+    //    /**
+    //     * @param uri The Uri to check.
+    //     * @return Whether the Uri authority is MediaProvider.
+    //     */
+    //    public static bool IsMediaDocument(Uri uri)
+    //    {
+    //        return "com.android.providers.media.documents".Equals(uri.Authority, StringComparison.InvariantCultureIgnoreCase);
+    //    }
+
+    //    /**
+    //     * @param uri The Uri to check.
+    //     * @return Whether the Uri authority is Google Photos.
+    //     */
+    //    public static bool IsGooglePhotosUri(Uri uri)
+    //    {
+    //        return "com.google.android.apps.photos.content".Equals(uri.Authority, StringComparison.InvariantCultureIgnoreCase);
+    //    }
+    //}
+}
+
--- Xamarin.ExifLocation/MainActivity.cs
+++ Xamarin.ExifLocation/MainActivity.cs
@@ -1,19 +1,193 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android;
+using Android.Content.PM;
+using Android.Util;
+using Android.Gms.Location;
+using System.Threading.Tasks;
+using Android.Locations;
+using Android.Provider;
+using Java.IO;
+using System.Linq;
+using Android.Media;
+using Android.Net;
 
 namespace Xamarin.ExifLocation
 {
     [Activity(Label = "Xamarin.ExifLocation", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        private const int SelectImageRequest = 67676;
+        private const int GetLocationRequest = 67677;
+        private const int WriteExternalStorageRequest = 67678;
+
+        private Location deviceLocation;
+        private Uri imageUri;
+
+        protected async override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.get_picture).Click += delegate { SelectImage(); };
+            FindViewById<Button>(Resource.Id.set_location).Click += async delegate { await SetLocation(); };
+
+            await GetLocation();
         }
+
+        #region Get location
+
+        private async Task GetLocation()
+        {
+            if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) == Permission.Granted)
+            {
+                var locationClient = LocationServices.GetFusedLocationProviderClient(this);
+                deviceLocation = await locationClient.GetLastLocationAsync();
+                if (deviceLocation != null)
+                {
+                    FindViewById<TextView>(Resource.Id.deviceLocationText).Text = deviceLocation.ToFormattedString(this);
+                }
+                else
+                {
+                    ShowError("Could not get location");
+                }
+            }
+            else
+            {
+                RequestPermissions(new[] { Manifest.Permission.AccessFineLocation }, GetLocationRequest);
+            }
+        }
+
+        public async override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            switch (requestCode)
+            {
+                case GetLocationRequest:
+                    if (grantResults.All(result => result == Permission.Granted))
+                    {
+                        await GetLocation();
+                    }
+                    else
+                    {
+                        ShowError("Get GPS location permission not granted");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Get image
+
+        private void SelectImage()
+        {
+            FindViewById<Button>(Resource.Id.set_location).Visibility = ViewStates.Invisible;
+            var intent = new Intent(Intent.ActionGetContent, MediaStore.Images.Media.ExternalContentUri);
+            intent.SetType("image/*");
+            intent.AddCategory(Intent.CategoryOpenable);
+
+            StartActivityForResult(intent, SelectImageRequest);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            switch (requestCode)
+            {
+                case SelectImageRequest:
+                    if (resultCode == Result.Ok)
+                    {
+                        ShowImage(data.Data);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ShowImage(Uri data)
+        {
+            imageUri = data;
+
+            var exif = new ExifInterface(ContentResolver.OpenInputStream(imageUri));
+            var location = exif.ReadLocation();
+            FindViewById<TextView>(Resource.Id.imageLocationText).Text = location.ToFormattedString(this);
+
+            FindViewById<ImageView>(Resource.Id.selected_image).SetImageURI(imageUri);
+            FindViewById<Button>(Resource.Id.set_location).Visibility = ViewStates.Visible;
+        }
+
+        #endregion
+
+        #region Set location
+
+        private async Task SetLocation()
+        {
+            var outfile = new File(GetPicturesDirectory(), $"{System.Guid.NewGuid()}.jpg");
+            var uri = Uri.FromFile(outfile);
+
+            await CreateCopy(imageUri, uri);
+            var exif = new ExifInterface(outfile.AbsolutePath);
+            exif.WriteLocation(deviceLocation);
+            MakeAvailableInGallery(uri);
+
+            FindViewById<TextView>(Resource.Id.imageLocationText).Text = $"{deviceLocation.Latitude}, {deviceLocation.Longitude}";
+        }
+
+        private async Task CreateCopy(Uri sourceUri, Uri destinationUri)
+        {
+            using (var source = new FileInputStream(ContentResolver.OpenFileDescriptor(sourceUri, "r").FileDescriptor))
+            {
+                using (var destination = new FileOutputStream(ContentResolver.OpenFileDescriptor(destinationUri, "w").FileDescriptor))
+                {
+                    byte[] content = new byte[source.Available()];
+                    await source.ReadAsync(content);
+                    await destination.WriteAsync(content);
+                    destination.Close();
+                }
+            }
+        }
+
+        private void MakeAvailableInGallery(Uri uri)
+        {
+            var mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            mediaScanIntent.SetData(uri);
+
+            SendBroadcast(mediaScanIntent);
+        }
+
+        private File GetPicturesDirectory()
+        {
+            var dir = new File(
+                Environment.GetExternalStoragePublicDirectory(Environment.DirectoryPictures),
+                PackageName);
+            if (!dir.Exists())
+            {
+                dir.Mkdirs();
+            }
+            return dir;
+        }
+
+        private void ShowError(string message)
+        {
+            Log.Error(PackageName, message);
+            new AlertDialog.Builder(this)
+                .SetMessage(message)
+                .SetNeutralButton("Ok", delegate { })
+                .Show();
+        }
+
+        #endregion
     }
 }
 
